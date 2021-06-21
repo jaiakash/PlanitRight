f6a09e7f488720110700e030f7fc1347d945a764
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-06-22 03:27:28 +0530

    Show current time with am/pm

--- web.js
+++ web.js
@@ -1,16 +1,28 @@
 function startTime() {
     var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('time').innerHTML =
-    h + ":" + m + ":" + s;
+    var hrs = today.getHours();
+    var min = checkTime(today.getMinutes());
+    var sec = checkTime(today.getSeconds());
+    var ampm = AmPm(today.getHours());
+    document.getElementById('time').innerHTML = hrs + ":" + min + ":" + sec + " " + ampm;
     var t = setTimeout(startTime, 1000);
   }
-  
+
   function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
+  }
+
+  function AmPm(hours){
+    var meridiem = "AM";
+    if (hours > 12) {
+        hours = hours - 12;
+        meridiem = "PM";
+    }
+
+    // 0 AM and 0 PM should read as 12
+    if (hours === 0) {
+        hours = 12;    
+    }
+    return meridiem;
   }
