867ff7c6b7a2199301161aecb47f09c4740417f3
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-06-22 21:43:15 +0530

    Finished Calender part

--- web.js
+++ web.js
@@ -1,28 +1,80 @@
 var  date = new Date();
 
-var monthDays = document.querySelector('.days')
+var render = () => {
 
-var months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+  date.setDate(1)
+  var monthDays = document.querySelector('.days')
 
-document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-document.querySelector(".date p").innerHTML = new Date().toDateString();
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate()
 
-let days = ""
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate()
 
-for(var i=1;i<=31;i++){
-  days += `<div>${i}</div>`
+  const firstDayIndex = date.getDay()
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay()
+
+  const nextDays = 7 - lastDayIndex - 1
+
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = ""
+
+  for (var x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+  }
+
+  for(var i=1;i<=lastDay;i++){
+    if(i===new Date().getDate() && date.getMonth()===new Date().getMonth()) {
+      days += `<div class="today">${i}</div>`
+    }
+    else {
+      days += `<div>${i}</div>`
+    }
+  }
+
+  for (var j=1;j<=nextDays;j++) {
+    days += `<div class="next-date">${j}</div>`
+  }
   monthDays.innerHTML = days
+
 }
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1)
+  render()
+})
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1)
+  render()
+})
+
+render()
